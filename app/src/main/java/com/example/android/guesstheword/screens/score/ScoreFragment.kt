12fd03a8a96e4f967bf82113a7d8b76b30384c63
Eliezer Sant Ana <eliezer.santana@t-systems.com.br>
2019-11-06 08:09:31 -0300

    adding viewModel to databinding

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -61,7 +61,7 @@ class ScoreFragment : Fragment() {
             binding.scoreText.text = newScore.toString()
         })
 
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
+        binding.scoreViewModel = viewModel
 
         viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
             if (playAgain) {
